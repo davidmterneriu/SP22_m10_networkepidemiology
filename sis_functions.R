0c197febcb2c1ccbb556ea099b04d0a75a835c27
davidmterneriu <davidmterner@gmail.com>
2022-03-30 23:30:08 -0400

    Added new SIS function and script.

---
+++ sis_functions.R
@@ -0,0 +1,59 @@
+#SIS Functions 
+
+#rm(list=ls())
+
+library(tidyverse)
+library(ggplot2)
+library(igraph)
+
+infect_share=function(G){
+  return(mean(V(G)$infect))
+}
+
+
+compartment_mod=function(G,beta,mu,init_infec=0.1,tmax=100){
+  #browser()
+  n_nodes=length(V(G))
+  G=G%>%set_vertex_attr(name="infect",value=0)
+  infect_nodes=round(init_infec*n_nodes)
+  node_ids_init_infect=sample(V(G),size = infect_nodes)%>%as.numeric()
+  
+  G=G%>%set_vertex_attr(name="infect",node_ids_init_infect,value=1)
+  G=G%>%set_vertex_attr(name="infect2",value=NA)
+  
+  
+  k_mean=mean(degree(G))
+  k2_mean=mean(degree(G)^2)
+  ratio1=(k2_mean-k_mean)/k_mean
+  r0_1=beta/mu
+  r0_real=r0_1*ratio1
+
+  
+  infect_share_df=data.frame(t=seq(0,tmax),infect_share=NA)
+  infect_share_df$infect_share[1]<-infect_share(G)
+  
+  for (t in 2:(tmax+1)){
+    
+    current_infect=which(V(G)$infect==1)
+    current_susp=which(V(G)$infect==0)
+    #Simulating Recovery 
+    recover_vals=as.numeric((rbinom(n=length(current_infect),size = 1,prob = mu)==0))
+    G=G%>%set_vertex_attr(name="infect2",current_infect,value=recover_vals)
+    num_infect_neigh=sapply(X=current_susp,FUN = function(x)sum(V(G)$infect[neighbors(G,v=x)]))
+    
+    no_infect_neigh=num_infect_neigh==0
+    new_infect=as.numeric(runif(n=length(current_susp))<(1 - (1 - beta)^num_infect_neigh))
+    new_infect[no_infect_neigh]<-0
+    
+    G=G%>%set_vertex_attr(name="infect2",current_susp,value=new_infect)
+    
+    #Initializing next iteration
+    V(G)$infect=V(G)$infect2
+    V(G)$infect2=NA
+    
+    infect_share_df$infect_share[t]=infect_share(G)
+  }
+
+  return(infect_share_df)
+ 
+}
--- sis_script.R
+++ sis_script.R
@@ -1,7 +1,39 @@
-#SIS model
+#SIS Demo
 
 rm(list=ls())
 
 library(tidyverse)
 library(ggplot2)
 library(igraph)
+
+source("sis_functions.R")
+
+g=igraph::erdos.renyi.game(n=100,p.or.m = 0.1)
+
+components <- igraph::clusters(g, mode="weak")
+biggest_cluster_id <- which.max(components$csize)
+
+# ids
+vert_ids <- V(g)[components$membership == biggest_cluster_id]
+
+# subgraph
+g1=igraph::induced_subgraph(g, vert_ids)
+
+trial_data=data.frame()
+niter=100
+
+for(i in 1:niter ){
+  print(i)
+  trial_data=rbind(trial_data,compartment_mod(G=g1,beta=0.5,mu=0.2)%>%
+    mutate(trial=i))
+  
+}
+
+
+trial_data%>%
+  group_by(t)%>%
+  summarise(infect_share=mean(infect_share))%>%
+  ggplot(aes(x=t,y=infect_share))+
+  geom_line()
+
+1-(0.2/(0.5*mean(degree(g1))))
